82183fd84e58e5313ea2f17e65a12ca6775ed36d
florencb <bradjflorence@gmail.com>
2016-04-08 12:55:57 -0400

    initial commit

---
+++ frmFutureValue.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmFutureValue
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtMonthlyInvestment = New System.Windows.Forms.TextBox()
+        Me.txtInterestRate = New System.Windows.Forms.TextBox()
+        Me.txtYears = New System.Windows.Forms.TextBox()
+        Me.txtFutureValue = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(27, 175)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 3
+        Me.btnCalculate.Text = "&Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(178, 175)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(24, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(99, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Monthly Investment"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(24, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(100, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Yearly Interest Rate"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(24, 96)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(86, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Number of Years"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(24, 131)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(67, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Future Value"
+        '
+        'txtMonthlyInvestment
+        '
+        Me.txtMonthlyInvestment.Cursor = System.Windows.Forms.Cursors.Default
+        Me.txtMonthlyInvestment.Location = New System.Drawing.Point(153, 26)
+        Me.txtMonthlyInvestment.Name = "txtMonthlyInvestment"
+        Me.txtMonthlyInvestment.Size = New System.Drawing.Size(100, 20)
+        Me.txtMonthlyInvestment.TabIndex = 0
+        '
+        'txtInterestRate
+        '
+        Me.txtInterestRate.Location = New System.Drawing.Point(153, 63)
+        Me.txtInterestRate.Name = "txtInterestRate"
+        Me.txtInterestRate.Size = New System.Drawing.Size(100, 20)
+        Me.txtInterestRate.TabIndex = 1
+        '
+        'txtYears
+        '
+        Me.txtYears.Location = New System.Drawing.Point(153, 97)
+        Me.txtYears.Name = "txtYears"
+        Me.txtYears.Size = New System.Drawing.Size(100, 20)
+        Me.txtYears.TabIndex = 2
+        '
+        'txtFutureValue
+        '
+        Me.txtFutureValue.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtFutureValue.Location = New System.Drawing.Point(153, 131)
+        Me.txtFutureValue.Name = "txtFutureValue"
+        Me.txtFutureValue.ReadOnly = True
+        Me.txtFutureValue.Size = New System.Drawing.Size(100, 20)
+        Me.txtFutureValue.TabIndex = 9
+        Me.txtFutureValue.TabStop = False
+        '
+        'frmFutureValue
+        '
+        Me.AcceptButton = Me.btnCalculate
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(284, 222)
+        Me.Controls.Add(Me.txtFutureValue)
+        Me.Controls.Add(Me.txtYears)
+        Me.Controls.Add(Me.txtInterestRate)
+        Me.Controls.Add(Me.txtMonthlyInvestment)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Name = "frmFutureValue"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Future Value"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtMonthlyInvestment As System.Windows.Forms.TextBox
+    Friend WithEvents txtInterestRate As System.Windows.Forms.TextBox
+    Friend WithEvents txtYears As System.Windows.Forms.TextBox
+    Friend WithEvents txtFutureValue As System.Windows.Forms.TextBox
+
+End Class
---
+++ frmFutureValue.vb
@@ -0,0 +1,28 @@
+﻿Public Class frmFutureValue
+    Dim futureValue As Decimal
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim monthlyInvestment As Decimal = CDec(txtMonthlyInvestment.Text)
+        Dim interestRate As Decimal = CDec(txtInterestRate.Text) / 12 / 100
+        Dim months As Integer = CInt(CDec(txtYears.Text) * 12)
+
+        'For i As Integer = 1 To months
+        '    futureValue = (futureValue + monthlyInvestment) * (1 + interestRate)
+        'Next i
+
+        Dim i As Integer = 1
+        Do While i <= months
+            futureValue = (futureValue + monthlyInvestment) * (1 + interestRate)
+            i += 1
+            If futureValue >= 1000 Then
+                Exit Do
+            End If
+        Loop
+
+        txtFutureValue.Text = FormatCurrency(futureValue)
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+End Class
